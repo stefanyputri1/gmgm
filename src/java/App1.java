4581deae124d7939ea7b450902694428e50f1446
stefanyputri1 <stefanyputri140@gmail.com>
2023-02-17 13:30:38 +0000

    fix bug in function (2023-02-17T13:30:38)

--- src/java/App1.java
+++ src/java/App1.java
@@ -1,3 +1,4 @@
 public class App64 { public static void main(String[] args) { System.out.println("update 2023-01-24T22:46:30"); } }
 public class App11 { public static void main(String[] args) { System.out.println("update 2023-02-03T02:47:36"); } }
 public class App11 { public static void main(String[] args) { System.out.println("update 2023-02-09T12:40:52"); } }
+public class App96 { public static void main(String[] args) { System.out.println("update 2023-02-17T13:30:38"); } }
