edb4258f958b812af17cd09debc39f6fba969da8
stefanyputri1 <stefanyputri140@gmail.com>
2023-02-20 01:28:14 +0000

    fix bug in function (2023-02-20T01:28:14)

--- src/go/main1.go
+++ src/go/main1.go
@@ -3,3 +3,4 @@ package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-01-23T07:29:0
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-02-09T02:53:38") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-02-09T03:53:12") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-02-13T20:25:40") }
+package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-02-20T01:28:14") }
